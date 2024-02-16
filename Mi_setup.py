cb63b873b7ab03b850e671c03bf9ca66949d9678
hectorpulido-svg <hemipulga@gmail.com>
2024-02-16 02:32:44 -0600

    solucion de errores #1

--- Mi_setup.py
+++ Mi_setup.py
@@ -10,8 +10,10 @@ import distutils
     -- python Mi_setup.py build --
 '''
 # C:\Users\hemip\AppData\Local\Programs\Python\Python310
-os.environ['TCL_LIBRARY'] = 'C:\\Users\hemip\\AppData\Local\\Programs\\Python\\Python310\\tcl\\tcl8.6'
-os.environ['TK_LIBRARY'] = 'C:\\Users\hemip\\AppData\Local\\Programs\\Python\\Python310\\tcl\\tk8.6'
+
+# ****** parece que hay error aquí *******
+# os.environ['TCL_LIBRARY'] = 'C:\\Users\hemip\\AppData\Local\\Programs\\Python\\Python310\\tcl\\tcl8.6'
+# os.environ['TK_LIBRARY'] = 'C:\\Users\hemip\\AppData\Local\\Programs\\Python\\Python310\\tcl\\tk8.6'
 
 base = None
 if sys.platform == 'win32':
