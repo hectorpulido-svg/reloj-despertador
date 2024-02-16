066147b05b84841ce4cbef665675ed248c84bfac
hectorpulido-svg <hemipulga@gmail.com>
2024-02-15 21:41:28 -0600

    usando media pleyer #2 correcto

--- modulos/__init__.py
+++ modulos/__init__.py
@@ -1,9 +1,9 @@
 # -*coding: utf-8 -*-
 import os
 from .alarmEntry import ALARM
-# from .systemclock import SYSTEMCLOCK
+from .systemclock import SYSTEMCLOCK
 from .player import PLAYER
-from .player_pgmixer import PLAYER
+# from .player_pgmixer import PLAYER
 from .slct_file import FILESELECTOR
 from .btn import CBUTTON
 from .systemdate import SYSDATE
