36441403e279c44f4b0b2d9e3fdccaab4962ffe3
hectorpulido-svg <hemipulga@gmail.com>
2024-02-16 02:21:16 -0600

    cambio en la ubicación de python

--- Mi_setup.py
+++ Mi_setup.py
@@ -9,9 +9,9 @@ import distutils
     ejecutar en la terminal como administrador el comando
     -- python Mi_setup.py build --
 '''
-
-os.environ['TCL_LIBRARY'] = 'C:\\Python39\\tcl\\tcl8.6'
-os.environ['TK_LIBRARY'] = 'C:\\Python39\\tcl\\tk8.6'
+# C:\Users\hemip\AppData\Local\Programs\Python\Python310
+os.environ['TCL_LIBRARY'] = 'C:\\Users\hemip\\AppData\Local\\Programs\\Python\\Python310\\tcl\\tcl8.6'
+os.environ['TK_LIBRARY'] = 'C:\\Users\hemip\\AppData\Local\\Programs\\Python\\Python310\\tcl\\tk8.6'
 
 base = None
 if sys.platform == 'win32':
